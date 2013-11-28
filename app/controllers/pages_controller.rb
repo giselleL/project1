af992423301c7ee77221b5314ef70b0e05d391fb
giselleL <gisellelingcon@gmail.com>
2013-11-28 22:56:16 +0800

    Fixed the views
    Working static pages, only needs design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
 	def index
-		items = Array.new
+		@items = Array.new
 		cake = Item.new("Cake", "Chocolate")
 		cookie = Item.new("Cookie", "Oatmeal")
 		cupcake = Item.new("Cupcake", "Caramel")
 		churros = Item.new("Churros", "Strawberry")
 		carrot = Item.new("Carrots", "Orange")
-		items.push(cake, cookie, cupcake, churros, carrot)
+		@items.push(cake, cookie, cupcake, churros, carrot)
 	end
 
 	def about
